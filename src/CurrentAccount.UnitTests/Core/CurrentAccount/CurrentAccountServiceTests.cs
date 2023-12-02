cd163ef923882306b40ca90167487346aa4125b9
Joao Perez <joaopaulosistemas.86@gmail.com>
2023-12-02 08:33:33 -0300

    Service unit tests not implemented

---
+++ src/CurrentAccount.UnitTests/Core/CurrentAccount/CurrentAccountServiceTests.cs
@@ -0,0 +1,24 @@
+﻿namespace CurrentAccount.UnitTests.Core.CurrentAccount
+{
+	public class CurrentAccountServiceTests
+	{
+		[Fact]
+		public async Task CreateNewAccountFromExistentCustomer_ValidData_ReturnsValidResult()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public async Task CreateNewAccountFromExistentCustomer_InvalidCustomerId_ReturnsErrorResult()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public async Task CreateNewAccountFromExistentCustomer_NegativeInitialCredit_ReturnsErrorResult()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
---
+++ src/CurrentAccount.UnitTests/Core/Customer/CustomerServiceTests.cs
@@ -0,0 +1,23 @@
+﻿namespace CurrentAccount.UnitTests.Core.Customer
+{
+	public class CustomerServiceTests
+	{
+		[Fact]
+		public async Task CreateCustomer_ValidData_ReturnsCustomerId()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public async Task GetCustomerById_ExistingCustomerId_ReturnsCustomerEntity()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public async Task GetCustomerById_NonExistingCustomerId_ReturnsNull()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
