e857302381543add4c164c730437259591aa63f2
Joao Perez <joaopaulosistemas.86@gmail.com>
2023-12-04 19:30:04 -0300

    Creating transaction services

---
+++ src/Transaction/CurrentAccount.Transaction.Application/Transactions/TransactionService.cs
@@ -0,0 +1,23 @@
+﻿using CurrentAccount.Core.Shared.Result;
+using CurrentAccount.Transaction.Core.Transactions;
+
+namespace CurrentAccount.Transaction.Application.Transactions
+{
+	public class TransactionService : ITransactionService
+	{
+		public Task<Guid> CreateTransaction(TransactionEntity transaction)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ResultModel<List<TransactionEntity>>> GetAllTransactionsFromAccount(Guid accountId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<decimal> GetLastBalanceFromAccount(Guid accountId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Transaction/CurrentAccount.Transaction.Core/Transactions/ITransactionService.cs
@@ -0,0 +1,11 @@
+﻿using CurrentAccount.Core.Shared.Result;
+
+namespace CurrentAccount.Transaction.Core.Transactions
+{
+	public interface ITransactionService
+	{
+		Task<Guid> CreateTransaction(TransactionEntity transaction);
+		Task<decimal> GetLastBalanceFromAccount(Guid accountId);
+		Task<ResultModel<List<TransactionEntity>>> GetAllTransactionsFromAccount(Guid accountId);
+	}
+}
