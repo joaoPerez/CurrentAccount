fd57826535d5aa51430af0a45064028b8789640b
Joao Perez <joaopaulosistemas.86@gmail.com>
2023-12-05 11:59:47 -0300

    Transaction handler

---
+++ src/Transaction/CurrentAccount.Transaction.Application/Transactions/Handlers/CreateTransactionHandler.cs
@@ -0,0 +1,21 @@
+﻿using CurrentAccount.Core.Shared.Result;
+using CurrentAccount.Transaction.Application.Transactions.Commands;
+using CurrentAccount.Transaction.Core.Transactions;
+
+namespace CurrentAccount.Transaction.Application.Transactions.Handlers
+{
+	public class CreateTransactionHandler : ICreateTransactionHandler
+	{
+		private readonly ITransactionService _transactionService;
+
+        public CreateTransactionHandler(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        public Task<ResultModel<Guid>> HandleCreateTransaction(CreateTransactionCommand command)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Transaction/CurrentAccount.Transaction.Application/Transactions/Handlers/ICreateTransactionHandler.cs
@@ -0,0 +1,10 @@
+﻿using CurrentAccount.Core.Shared.Result;
+using CurrentAccount.Transaction.Application.Transactions.Commands;
+
+namespace CurrentAccount.Transaction.Application.Transactions.Handlers
+{
+	public interface ICreateTransactionHandler
+	{
+		Task<ResultModel<Guid>> HandleCreateTransaction(CreateTransactionCommand command);
+	}
+}
