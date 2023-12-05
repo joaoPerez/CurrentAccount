39369b9c76538b6be4430d8f060afcc8a9a59468
Joao Perez <joaopaulosistemas.86@gmail.com>
2023-12-05 15:52:40 -0300

    Create TransactionContext.cs

---
+++ src/Transaction/CurrentAccount.Transaction.Infrastructure/Databases/Contexts/TransactionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CurrentAccount.Transaction.Infrastructure.Databases.Contexts
+{
+	public class TransactionContext : DbContext
+	{
+		public TransactionContext(DbContextOptions<TransactionContext> options)
+		: base(options)
+		{
+		}
+
+
+	}
+}
