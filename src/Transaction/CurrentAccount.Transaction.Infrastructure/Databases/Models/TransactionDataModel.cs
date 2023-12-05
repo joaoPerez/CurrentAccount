3601cea556b17c37247be925c9763ae7c161f645
Joao Perez <joaopaulosistemas.86@gmail.com>
2023-12-05 15:52:35 -0300

    Create TransactionDataModel.cs

---
+++ src/Transaction/CurrentAccount.Transaction.Infrastructure/Databases/Models/TransactionDataModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CurrentAccount.Transaction.Infrastructure.Databases.Models
+{
+	public class TransactionDataModel
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		public Guid CurrentAccountId { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string TransactionType { get; set; }
+
+		[Required]
+		[MaxLength(255)]
+		public string Description { get; set; }
+
+		[Column(TypeName = "decimal(6,2)")]
+		[Required]
+		public decimal ActualBalance { get; set; } = 0;
+
+		[Column(TypeName = "decimal(6,2)")]
+		[Required]
+		public decimal Amount { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string Currency { get; set; }
+
+		[Required]
+		public DateTime TransactionDate { get; set; } = DateTime.Now.ToUniversalTime();
+	}
+}
